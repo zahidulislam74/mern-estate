c3f21a6658cb12ea7415bafdc7cffadfcb00708d
Zahidul Islam <zahidulislamrupom651@gmail.com>
2023-10-16 08:11:20 +0600

    create user model

--- api/index.js
+++ api/index.js
@@ -2,12 +2,33 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
+import morgan from "morgan";
+import createError from "http-errors";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 const MongoDBUrl = process.env.MONGODB;
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 15 minutes
+  max: 10, // limit each IP to 10 requests per windowMs
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: "Too many requests from this IP, please try again later.",
+});
+
+app.use(limiter);
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/test", (req, res) => {
+  res.status(200).json({ message: "API is working fine" });
+});
 
 mongoose
   .connect(MongoDBUrl)
@@ -18,6 +39,19 @@ mongoose
     console.log(err.message);
   });
 
+//client error handling
+app.use((req, res, next) => {
+  next(createError(404, "Route not found"));
+});
+
+// server error handling
+app.use((err, req, res, next) => {
+  return res.status(err.status || 500).json({
+    success: false,
+    message: err.message,
+  });
+});
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
